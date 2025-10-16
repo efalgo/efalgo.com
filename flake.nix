ca061dee7081611daa339f6686e74ad868965701
Paschalis Bizopoulos <pbizop@gmail.com>
2025-10-16 18:00:08 +0300

    init

---
+++ flake.nix
@@ -0,0 +1,12 @@
+{
+  inputs = {
+    canonicalization.url = "github:pbizopoulos/canonicalization";
+    nixpkgs.follows = "canonicalization/nixpkgs";
+  };
+  outputs =
+    inputs:
+    inputs.canonicalization.blueprint { inherit inputs; }
+    // {
+      formatter.x86_64-linux = inputs.canonicalization.formatter.x86_64-linux;
+    };
+}
